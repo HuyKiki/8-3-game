42cdde102d5ac5441e45bee0d666e5fc039efc7b
Lê Gia Huy <166299433+HuyKiki@users.noreply.github.com>
2025-03-06 20:50:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,36 @@ const cauHoi = [
         hinhAnh: "https://nghiencuuquocte.org/wp-content/uploads/2022/06/16.jpg"
     },
     {
+        cauHoi: "Sự kiện nào là nguyên nhân chính dẫn đến việc thành lập ngày Quốc tế Phụ nữ 8/3?",
+        dapAn: ["Cuộc biểu tình đòi quyền bầu cử của phụ nữ ở Anh", "Cuộc đình công của nữ công nhân ngành dệt may tại Mỹ", "Cách mạng Pháp", "Chiến tranh thế giới thứ nhất"],
+        dapAnDung: "Cuộc đình công của nữ công nhân ngành dệt may tại Mỹ",
+        hinhAnh: "https://scontent.fhan9-1.fna.fbcdn.net/v/t1.15752-9/481972625_1831401007693415_5284344006175331551_n.gif?_nc_cat=104&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHiTNM0H2gyZ_fNABJDwLPx32AFH3zxNuLfYAUffPE24uuDcGSNWt_LoTiNbpfxM6FqoqM3NOF9e8xwl_GT47Ll&_nc_ohc=_M03pT4rtwIQ7kNvgEcjBKa&_nc_oc=AdgZdnmxeQsiBf1NJukYhZo_rIwqcVya9xVMSMrevSTgLuO-DArIswImJ1xRdZvdmUs&_nc_zt=23&_nc_ht=scontent.fhan9-1.fna&oh=03_Q7cD1wFQWVMew-kUnJpKzw2tHAXDyyYw93udMZbVUqn08CPTFQ&oe=67F11F66"
+    },
+    {
+        cauHoi: "Ngày 8/3 được chọn làm Ngày Quốc tế Phụ nữ từ năm nào?",
+        dapAn: ["1920", "1945", "1905", "1910"],
+        dapAnDung: "1910",
+        hinhAnh: "https://images.fineartamerica.com/images-medium-large-5/-1910s-usa-womens-the-advertising-archives.jpg"
+    },
+    {
+        cauHoi: "Ai là người đề xuất ý tưởng về Ngày Quốc tế Phụ nữ? (gợi ý: Người Đức)",
+        dapAn: ["Rosa Luxemburg", "Clara Zetkin", "Emmeline Pankhurst", "Susan B. Anthony"],
+        dapAnDung: "Clara Zetkin",
+        hinhAnh: "https://upload.wikimedia.org/wikipedia/commons/1/10/C_Zetkin_1.jpg"
+    },
+    {
+        cauHoi: "Ngày 8/3 được tổ chức lần đầu tiên tại Việt Nam vào năm nào?",
+        dapAn: ["1910", "1977", "1930", "2025"],
+        dapAnDung: "1930",
+        hinhAnh: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/800px-Flag_of_Vietnam.svg.png"
+    },
+    {
+        cauHoi: "Quốc gia nào sau đây coi ngày 8/3 là ngày lễ chính thức?",
+        dapAn: ["Việt Nam", "Mỹ", "Nhật Bản", "Đức"],
+        dapAnDung: "Việt Nam",
+        hinhAnh: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2F%40christalluster%2Fpunctuation-of-life-question-mark-a452d23d29b3&psig=AOvVaw29MjhWvNm9fsUtBwlehvm6&ust=1741355395974000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKihm7_M9YsDFQAAAAAdAAAAABAE"
+    },
+    {
         cauHoi: "Hoa nào thường được tặng trong ngày 8/3?",
         dapAn: ["Hoa lan", "Hoa cúc", "Hoa hồng", "Tất cả đều đúng"],
         dapAnDung: "Hoa hồng",
